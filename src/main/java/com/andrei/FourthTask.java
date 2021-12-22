7ab284f947d8dd78b8d0b5a862ed3e9a4554da9f
MashaMmmM <marymelnikova2000@gmail.com>
2021-12-22 16:04:11 +0300

    fix mistake in filename

--- src/main/java/com/andrei/FourthTask.java
+++ src/main/java/com/andrei/FourthTask.java
@@ -1,6 +1,6 @@
 package com.andrei;
 
-public class FoursTask {
+public class FourthTask {
 
 //    A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 //
--- src/test/java/com/andrei/FourthTaskTest.java
+++ src/test/java/com/andrei/FourthTaskTest.java
@@ -3,39 +3,39 @@ package com.andrei;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FoursTaskTest {
+public class FourthTaskTest {
     @Test
     public void calculate0() {
         int expected = 0;
-        int actual = FoursTask.calculate(1, 0);
+        int actual = FourthTask.calculate(1, 0);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void calculate1() {
         int expected = 0;
-        int actual = FoursTask.calculate(1, 1);
+        int actual = FourthTask.calculate(1, 1);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void calculate100() {
         int expected = 9009;
-        int actual = FoursTask.calculate(100, 1);
+        int actual = FourthTask.calculate(100, 1);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void calculate1000() {
         int expected = 906609;
-        int actual = FoursTask.calculate(1000, 2);
+        int actual = FourthTask.calculate(1000, 2);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void calculate10000() {
         int expected = 99000099;
-        int actual = FoursTask.calculate(10000, 3);
+        int actual = FourthTask.calculate(10000, 3);
         Assert.assertEquals(expected, actual);
     }
 
